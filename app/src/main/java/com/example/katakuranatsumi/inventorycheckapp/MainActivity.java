1fa0d29d821adb31443b3fa3bb4f982d86b8e045
Katakuranatsumi <usako7233@gmail.com>
2018-11-25 21:19:51 +0900

    2018/11/25 ListViewをMainActivityで生成

--- app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/MainActivity.java
+++ app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.katakuranatsumi.inventorycheckapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ListViewオブジェクトを取得
+        ListView inventList = findViewById(R.id.inventList);
+
+//        リストビューに表示するリストビュー用Listオブジェクトを作成
+        List<String> invent_list = new ArrayList<>();
+
+//          リストデータの登録
+        invent_list.add("遠足");
+        invent_list.add("スケート");
+
+//        アダプタオブジェクトを生成
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,
+                invent_list);
+
+//        リストビューにアダプタオブジェクトを生成
+        inventList.setAdapter(adapter);
 
         ImageButton button = findViewById(R.id.imageButton2);
         focusListener listener = new focusListener();

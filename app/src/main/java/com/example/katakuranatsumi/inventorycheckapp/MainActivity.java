98a056c3fda73b1946df99dcf052eb573e799346
Katakuranatsumi <usako7233@gmail.com>
2018-11-25 18:02:05 +0900

    2018c/11/25 新規登録ボタンのクリックイベント実装

--- app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/MainActivity.java
+++ app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton button = findViewById(R.id.imageButton2);
+        focusListener listener = new focusListener();
+        button.setOnClickListener(listener);
     }
 
-
+//    新規登録ボタンをクリックした時のリスナクラス
+    private class focusListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+        Toast toast = Toast.makeText(MainActivity.this, "フォーカス", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
 }

97c145f03fe45441b5fc9afbf2ca66b383735273
Katakuranatsumi <usako7233@gmail.com>
2018-12-12 16:13:49 +0900

    2018/12/5　イベント名と持ち物名をデータベースに保存する処理

--- app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/NewInventActivity.java
+++ app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/NewInventActivity.java
@@ -1,18 +1,17 @@
 package com.example.katakuranatsumi.inventorycheckapp;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class NewInventActivity extends AppCompatActivity {
 
     Button bt_save;
+    int _inventID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +19,14 @@ public class NewInventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_invent);
 
 //      日付記入欄のクリックイベントのためのリスナー作成
-        EditText editdate = findViewById(R.id.inventdate);
         dateListener listener = new dateListener();
-        editdate.setOnClickListener(listener);
+        EditText editDate = findViewById(R.id.inventDate);
+        editDate.setOnClickListener(listener);
 
-//      持ち物追加ボタンのクリックイベントのためのリスナー作成
-        ImageButton addInventButton = findViewById(R.id.add_invent);
-        addInventListener listener1 = new addInventListener();
-        addInventButton.setOnClickListener(listener1);
+////      持ち物追加ボタンのクリックイベントのためのリスナー作成
+//        ImageButton addInventButton = findViewById(R.id.add_invent);
+//        addInventListener listener1 = new addInventListener();
+//        addInventButton.setOnClickListener(listener1);
 
 //      保存ボタンを取得
         bt_save = findViewById(R.id.bt_save);
@@ -44,15 +43,13 @@ public class NewInventActivity extends AppCompatActivity {
         }
     }
 
-    private class addInventListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            System.out.println("持ち物リスト追加");
-////              持ち物リストを追加する処理
-//            layout.addView(editText);
-        }
-    }
+//    private class addInventListener implements View.OnClickListener{
+//
+//        @Override
+//        public void onClick(View v) {
+//            System.out.println("持ち物リスト追加");
+//        }
+//    }
 
 //  [保存] ボタンを押した時のイベント
     public void onSaveButtonClick(View view){
@@ -60,11 +57,46 @@ public class NewInventActivity extends AppCompatActivity {
         EditText titleText = findViewById(R.id.inventTitle);
         String title = titleText.getText().toString();
 
+//        持ち物欄を取得
+        EditText inventData = findViewById(R.id.newInvent);
+        String invent = inventData.getText().toString();
+
+
 //        データベースヘルパーオブジェクトを作成
         DatabaseHelper helper = new DatabaseHelper(NewInventActivity.this);
 
 //        データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
         SQLiteDatabase db = helper.getWritableDatabase();
-    }
 
+        try {
+//        はじめに持ち物リストのデータを削除。そのあとINSERTを行う
+//        削除用SQL文字列を用意
+         String sqlDelete = "DELETE FROM inventlist WHERE _id = ?";
+
+//         for (int i = -1; i < 0; ++i) {
+
+////        SQL文字列を元にプリペアドステートメントを取得
+         SQLiteStatement stmt = db.compileStatement(sqlDelete);
+//        変数のバインド
+        stmt.bindLong(1, _inventID);
+        stmt.executeUpdateDelete();
+
+//       インサート用SQL文字列の用意。
+         String sqlInsert = "INSERT INTO inventlist VALUES (?, ?, ?)";
+//       SQL文字列を元にプリペアドステートメントを取得
+         stmt = db.compileStatement(sqlInsert);
+
+
+//        変数のバインド
+             stmt.bindLong(1, _inventID);
+             stmt.bindString(2, title);
+             stmt.bindString(3, invent);
+//        インサートSQLの実行
+             stmt.executeInsert();
+//         }
+       }
+    finally {
+        db.close();
+       }
+      }
 }

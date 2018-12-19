b43717ca5b15d3c3c349c56873be4eeb8624df34
Katakuranatsumi <usako7233@gmail.com>
2018-12-19 15:25:53 +0900

    2018/12/18 TOP画面から選択した持ち物リストをDBから呼び出し

--- app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/ListDatail.java
+++ app/src/main/java/com/example/katakuranatsumi/inventorycheckapp/ListDatail.java
@@ -3,6 +3,8 @@ package com.example.katakuranatsumi.inventorycheckapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -20,7 +23,10 @@ public class ListDatail extends Fragment {
 
     //    このフラグメントが所蔵するアクティビティオブジェクト
     private Activity _parentActivity;
-    int _inventID;
+    private int _inventID;
+
+    private String title = "";
+    private String date = "";
 
 
     @Override
@@ -34,18 +40,63 @@ public class ListDatail extends Fragment {
         Bundle extras;
         extras = intent.getExtras();
 
-        String title = extras.getString("title");
+        title = extras.getString("title");
+        date = extras.getString("date");
+        _inventID = extras.getInt("position");
+        _inventID = _inventID + 1;
 
         Log.d("インベントタイトル", title);
+        Log.d("インベント日付", date);
 
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_datail, container, false);
 
-        return inflater.inflate(R.layout.fragment_list_datail, container, false);
+//      データベースヘルパーオブジェクトを作成
+        DatabaseHelper helper = new DatabaseHelper(_parentActivity);
+//      データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        try {
+//      主キーによる検索SQL文字列の用意
+            String sql = "SELECT * FROM inventlist WHERE _id = ?";
+            String[] params = {String.valueOf(_inventID)};
+//      SQLの実行
+            Cursor cursor = db.rawQuery(sql, params);
+
+            String invent = "";
+
+            boolean isEofF = cursor.moveToNext();
+
+//      SQL実行の戻り値であるカーソルオブジェクトをループさせてデータベース内のデータを取得
+        while (isEofF) {
+//         選択したリストのインデックス値を取得
+                int inventIndex = cursor.getColumnIndex("invent");
+                invent = cursor.getString(inventIndex);
+                Log.d("持ち物リスト", invent);
+
+//              一回でループを止めるためisEofFをfalseに設定
+                isEofF = false;
+            }
+            TextView listInvent = view.findViewById(R.id.list_invent);
+            listInvent.setText(invent);
+            }
+            finally{
+                db.close();
+            }
+
+//      Top画面で選択したタイトルのデータを取得
+        TextView listTitle = view.findViewById(R.id.list_title);
+        listTitle.setText(title);
+
+//      Tp画面で選択した日付データの取得
+        TextView listDate = view.findViewById(R.id.list_date);
+        listDate.setText(date);
+
+        return view;
     }
 
 }
